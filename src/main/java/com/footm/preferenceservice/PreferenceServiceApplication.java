a5359faf23c4f8c7e6353e58752fad05ba75cf51
RinorSefa <rinorsefa@hotmail.com>
2020-04-29 23:18:50 +0200

    initialisation

---
+++ src/main/java/com/footm/preferenceservice/PreferenceServiceApplication.java
@@ -0,0 +1,34 @@
+package com.footm.preferenceservice;
+
+import io.dropwizard.Application;
+import io.dropwizard.jdbi3.JdbiFactory;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+import org.jdbi.v3.core.Jdbi;
+
+public class PreferenceServiceApplication extends Application<PreferenceServiceConfiguration> {
+
+    public static void main(final String[] args) throws Exception {
+        new PreferenceServiceApplication().run(args);
+    }
+
+    @Override
+    public String getName() {
+        return "PreferenceService";
+    }
+
+    @Override
+    public void initialize(final Bootstrap<PreferenceServiceConfiguration> bootstrap) {
+        // TODO: application initialization
+    }
+
+    @Override
+    public void run(final PreferenceServiceConfiguration configuration,
+                    final Environment environment) {
+
+        //database
+        final JdbiFactory factory = new JdbiFactory();
+        final Jdbi jdbi = factory.build(environment, configuration.getDataSourceFactory(), "postgresql");
+        environment.jersey().register(jdbi);
+    }
+}
---
+++ src/main/java/com/footm/preferenceservice/PreferenceServiceConfiguration.java
@@ -0,0 +1,27 @@
+package com.footm.preferenceservice;
+
+import io.dropwizard.Configuration;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.dropwizard.db.DataSourceFactory;
+import org.hibernate.validator.constraints.*;
+
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+
+public class PreferenceServiceConfiguration extends Configuration {
+//DATABASE CONFIGURATION
+
+    @Valid
+    @NotNull
+    private DataSourceFactory database = new DataSourceFactory();
+
+    @JsonProperty("database")
+    public void setDataSourceFactory(DataSourceFactory factory) {
+        this.database = factory;
+    }
+
+    @JsonProperty("database")
+    public DataSourceFactory getDataSourceFactory() {
+        return database;
+    }
+}
